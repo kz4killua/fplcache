8e8181ee8af7c157b2f714cbc56d1e2d24b77c7c
Ifeanyi Obinelo <62487964+kz4killua@users.noreply.github.com>
2022-08-27 22:17:21 +0100

    Update cache.py

--- cache.py
+++ cache.py
@@ -48,40 +48,6 @@ def main():
         json.dump(last_checked_event, f) 
         
     return
-    
-    
-    
-    url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
-    cache = Path('data')
-    
-    season = '2022-23'
-    DATA_DIR = Path(f'data/api/{season}/players')
-    DATA_DIR.mkdir(parents=True, exist_ok=True)
-   
-    # Fetch the FPL bootstrap JSON.
-    print(f'Fetching {url}... ', end='', flush=True)
-    r = requests.get(url)
-    if not r.ok:
-        print(f'Failed: {r.status_code}.')
-        exit(1)
-    print('OK.')
-
-    # Prettify the JSON so it can be diffed.
-    print(f'Prettifying JSON... ', end='', flush=True)
-    pretty = json.dumps(r.json(), indent=4, sort_keys=True)
-    print(f'OK.')
-
-    # Prepare the cache file path.
-    now = datetime.datetime.today()
-    # path = cache / Path(f'{now.year}/{now.month}/{now.day}/{now.hour:02d}{now.minute:02d}.json.xz')
-    path = DATA_DIR / Path(f'{now.hour:02d}{now.minute:02d}.json.xz')
-    # path.parent.mkdir(parents=True, exist_ok=True)
-
-    # Compress the JSON into the cache.
-    print(f'Compressing into {path}... ', end='', flush=True)
-    with lzma.open(path, 'wt', encoding='utf-8') as f:
-        f.write(pretty)
-    print(f'OK.')
 
 if __name__ == '__main__':
     main()
