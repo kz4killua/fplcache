cdad6db648e36b43c66dd2666db05852d7a51e72
Ifeanyi Obinelo <62487964+kz4killua@users.noreply.github.com>
2022-08-27 22:11:08 +0100

    Update cache.py

--- cache.py
+++ cache.py
@@ -24,6 +24,33 @@ def main():
     elements = pd.DataFrame(general_data['elements'])
     events = pd.DataFrame(general_data['events'])
     
+    # Prepare the data directory
+    DATA_DIR = Path(f'data/api/{season}/players')
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    last_checked_event = data.get_last_checked_event(events)
+    last_checked_path = DATA_DIR / 'last_checked.json'
+    
+    # Quit if the data is already up to date
+    if last_checked_path.exists():
+        with open(last_checked_path, 'r') as f:
+            if json.load(f) == last_checked_event:
+                return
+
+    # Update player data.   
+    for id in elements['id'].unique():
+        player = api.get_player_data(id)
+        player = pd.DataFrame(data['history'])
+        player.to_csv(DATA_DIR / f'{id}.csv', index=False)
+
+    # Keep track of when last we saved player data.
+    with open(last_checked_path, 'w') as f:
+        json.dump(last_checked_event, f) 
+        
+    return
+    
+    
+    
     url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
     cache = Path('data')
     
